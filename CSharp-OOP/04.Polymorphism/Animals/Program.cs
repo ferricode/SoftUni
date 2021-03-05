9b160252441e22d147fce31ad49287af1964fa70
fairyCountess <ferided88@gmail.com>
2021-03-05 20:42:13 +0200

    Adding Polimorphism lecture to C# OOP.

---
+++ CSharp-OOP/04.Polymorphism/Animals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Cat("Koti", "Negatiwni emocii");
+            Console.WriteLine(animal.ExplainSelf());
+
+            animal = new Dog("Ku4ee", "Pozitivni emocii");
+            Console.WriteLine(animal.ExplainSelf());
+        }
+    }
+}
