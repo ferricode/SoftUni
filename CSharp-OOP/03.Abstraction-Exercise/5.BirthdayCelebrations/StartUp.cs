e3740c549f04c224b1eb6b1c74a1f76749b2a912
fairyCountess <ferided88@gmail.com>
2021-03-05 20:44:30 +0200

    Adding Abstraction Exercise to C# OOP.

---
+++ CSharp-OOP/03.Abstraction-Exercise/5.BirthdayCelebrations/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<IBirthable> birthables = new List<IBirthable>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "End")
+                {
+                    break;
+                }
+                string[] parts = line.Split();
+                string type = parts[0];
+
+                if (type == nameof(Citizen))
+                {
+                    string name = parts[1];
+                    int age = int.Parse(parts[2]);
+                    string id = parts[3];
+                    string birthable = parts[4];
+
+                    birthables.Add(new Citizen(name, age, id, birthable));
+                }
+                else if (type == nameof(Pet))
+                {
+                    string name = parts[1];
+                    string birthdate = parts[2];
+
+                    birthables.Add(new Pet(name, birthdate));
+                }
+
+            }
+            string filterYear = Console.ReadLine();
+
+            List<IBirthable> filtered = birthables.Where(b=>b.Birthdate.EndsWith(filterYear)).ToList();
+
+            foreach (var birthable in filtered)
+            {
+                Console.WriteLine(birthable.Birthdate);
+            }
+            
+
+        }
+    }
+}
