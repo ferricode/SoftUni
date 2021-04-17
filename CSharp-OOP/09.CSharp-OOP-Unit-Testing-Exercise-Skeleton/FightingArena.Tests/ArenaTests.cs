71c13d3d8641f8df0a3d6bd7987e2f78ae0cb5b5
fairyCountess <ferided88@gmail.com>
2021-04-17 17:46:37 +0300

    Add Unit testing Exercises, Fighting Arena problem, to C#OOP.

--- CSharp-OOP/09.CSharp-OOP-Unit-Testing-Exercise-Skeleton/FightingArena.Tests/ArenaTests.cs
+++ CSharp-OOP/09.CSharp-OOP-Unit-Testing-Exercise-Skeleton/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,91 @@
-﻿using NUnit.Framework;
+﻿using FightingArena;
+using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace Tests
 {
     public class ArenaTests
     {
+        private Arena arena;
         [SetUp]
         public void Setup()
         {
+            this.arena = new Arena();
         }
 
         [Test]
-        public void Test1()
+        public void Ctor_InitializeWarriors()
         {
-            Assert.Pass();
+            Assert.That(this.arena.Warriors, Is.Not.Null);
         }
+        [Test]
+        public void Count_IsZero_WhenArenaIsEmpty()
+        {
+            Assert.That(this.arena.Count, Is.EqualTo(0));
+        }
+        [Test]
+        public void Enroll_ThrowException_WhenWarriorAlreadyExist()
+        {
+            string name = "Warrior";
+            this.arena.Enroll(new Warrior(name, 50, 50));
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Enroll(new Warrior(name, 60, 70)));
+        }
+        [Test]
+        public void Enroll_IncreasesCountWhenAddsWarrior()
+        {
+
+            this.arena.Enroll(new Warrior("Warrior", 50, 50));
+            Assert.That(this.arena.Count, Is.EqualTo(1));
+        }
+        [Test]
+        public void Enroll_AddsWarriorToWarriors()
+        {
+            string name = "Warrior";
+
+            this.arena.Enroll(new Warrior(name, 50, 50));
+
+            Assert.That(arena.Warriors.Any(w => w.Name == name), Is.True);
+        }
+        [Test]
+        public void Fight_ThrowsException_WhenAttackerDoesNotExist()
+        {
+            string attacker = "Attacker";
+
+            this.arena.Enroll(new Warrior(attacker, 50, 50));
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight(attacker, "Defender"));
+        }
+        [Test]
+        public void Fight_ThrowsException_WhenDefenderDoesNotExist()
+        {
+            string defender = "Deffender";
+
+            this.arena.Enroll(new Warrior(defender, 50, 50));
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Attacker", defender));
+        }
+        [Test]
+        public void Fight_ThrowsException_WhenDefenderAndAttackerDoesNotExists()
+        {
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Attacker", "Deffender"));
+        }
+        [Test]
+        public void Fight_BothWarriorsLoseHpInFight()
+        {
+            var initialHp = 100;
+            var attacker = new Warrior("Attacker", 50, initialHp);
+            var deffender = new Warrior("Deffender", 50, initialHp);
+
+            this.arena.Enroll(attacker);
+            this.arena.Enroll(deffender);
+            this.arena.Fight(attacker.Name, deffender.Name);
+
+            Assert.That(attacker.HP,Is.EqualTo(initialHp-deffender.Damage));
+            Assert.That(deffender.HP,Is.EqualTo(initialHp-attacker.Damage));
+        }
+
     }
 }
--- CSharp-OOP/09.CSharp-OOP-Unit-Testing-Exercise-Skeleton/FightingArena.Tests/WarriorTests.cs
+++ CSharp-OOP/09.CSharp-OOP-Unit-Testing-Exercise-Skeleton/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,74 @@
 using NUnit.Framework;
+using FightingArena;
+using System;
 
 namespace Tests
 {
     public class WarriorTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+
 
         [Test]
-        public void Test1()
+        [TestCase("", 50, 100)]
+        [TestCase(" ", 50, 100)]
+        [TestCase(null, 50, 100)]
+        [TestCase("Warrior", 0, 100)]
+        [TestCase("Warrior", -50, 100)]
+        [TestCase("Warrior", 50, -100)]
+        public void CTor_ThrowsException_WhenDataIsInvalid(string name, int damage, int hp)
         {
-            Assert.Pass();
+            Assert.Throws<ArgumentException>(() => new Warrior(name, damage, hp));
+        }
+        [Test]
+        [TestCase(30, 55)]
+        [TestCase(15, 55)]
+        [TestCase(55, 30)]
+        [TestCase(55, 15)]
+        public void Attack_ThrowsException_WhenHpIsLessThanMinimum(int attackerHp, int warriorHP)
+        {
+            var attacker = new Warrior("Attacker", 50, attackerHp);
+            var warrior = new Warrior("Attacker", 10, warriorHP);
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(warrior));
+        }
+        [Test]
+
+        public void Attack_ThrowsException_WhenWarriorKillsTheAttacker()
+        {
+            var attacker = new Warrior("Attacker", 50, 100);
+            var warrior = new Warrior("Attacker", attacker.HP + 2, 100);
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(warrior));
+        }
+        [Test]
+
+        public void Attack_DecreasesAttackerHealthPoint()
+        {
+            int initialAttackerHp = 100;
+            var attacker = new Warrior("Attacker", 50, initialAttackerHp);
+            var warrior = new Warrior("Attacker", 30, 100);
+            attacker.Attack(warrior);
+
+            Assert.That(attacker.HP, Is.EqualTo(initialAttackerHp - warrior.Damage));
+        }
+        [Test]
+
+        public void Attack_SetEnemyHpToZero_WhenAttackerDamageIsGreaterThanEnemyHp()
+        {
+
+            var attacker = new Warrior("Attacker", 50, 100);
+            var warrior = new Warrior("Attacker", 30, 40);
+            attacker.Attack(warrior);
+
+            Assert.That(warrior.HP, Is.EqualTo(0));
+        }
+        [Test]
+        public void Attack_DecreasesEnemyHpByAtacckerDamage()
+        {
+            int warriorInitialHp = 100;
+            Warrior attacker = new Warrior("Attacker", 50, 100);
+            Warrior warrior = new Warrior("Attacker", 30, warriorInitialHp);
+            attacker.Attack(warrior);
+
+            Assert.That(warrior.HP, Is.EqualTo(warriorInitialHp - attacker.Damage));
         }
     }
 }
