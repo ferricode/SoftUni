3ef3b7a3c8bda6f2ea335b5bcb9be53bc2204bad
ferricode <ferided88@gmail.com>
2022-02-14 00:14:02 +0200

    View templating added.

--- CSharp-Web/BasicWebServer/BasicWebServer.Server/Responses/ViewResponse.cs
+++ CSharp-Web/BasicWebServer/BasicWebServer.Server/Responses/ViewResponse.cs
@@ -10,7 +10,7 @@ namespace BasicWebServer.Server.Responses
     public class ViewResponse : ContentResponse
     {
         private const char PathSeparator = '/';
-        public ViewResponse(string viewName, string controllerName)
+        public ViewResponse(string viewName, string controllerName, object model=null)
             : base(string.Empty, ContentType.Html)
         {
             if (!viewName.Contains(PathSeparator))
@@ -20,7 +20,32 @@ namespace BasicWebServer.Server.Responses
             var viewPath = Path.GetFullPath($"./Views/{viewName.TrimStart(PathSeparator)}.cshtml");
             var viewContent = File.ReadAllText(viewPath);
 
+            if (model!=null)
+            {
+                viewContent = PopulateModel(viewContent, model);
+            }
             Body = viewContent;
+        }
+
+        private string PopulateModel(string viewContent, object model)
+        {
+            var data = model
+                  .GetType()
+                  .GetProperties()
+                  .Select(p => new
+                  {
+                      p.Name,
+                      Value = p.GetValue(model)
+                  });
+
+            foreach (var item in data)
+            {
+                const string openingBrackets = "{{";
+                const string closingBrackets = "}}";
+                viewContent=viewContent.Replace($"{openingBrackets}{item.Name}{closingBrackets}", item.Value.ToString());
+            }
+
+            return viewContent;
         }
     }
 }
