f6da242e9b284b02951c7afc0770eaa0995059e9
ferricode <ferided88@gmail.com>
2022-02-13 23:01:38 +0200

    C# Web BasicWebServer project, added HomeController and UserController.

---
+++ CSharp-Web/BasicWebServer/BasicWebServer.Server/HTTP/HeaderCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class HeaderCollection : IEnumerable<Header>
+    {
+        private readonly Dictionary<string, Header> headers;
+
+        public HeaderCollection()
+            => this.headers = new Dictionary<string, Header>();
+
+        public string this[string name]
+          => this.headers[name].Value;
+
+        public int Count => this.headers.Count;
+
+        public bool Contains(string name)
+          => this.headers.ContainsKey(name);
+
+        internal void Add(string contentType, object fileContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string name, string value)
+            => this.headers[name] = new Header(name, value);
+
+        public IEnumerator<Header> GetEnumerator()
+            => this.headers.Values.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
---
+++ CSharp-Web/BasicWebServer/BasicWebServer.Server/Routing/IRoutingTable.cs
@@ -0,0 +1,15 @@
+﻿using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Routing
+{
+    public interface IRoutingTable
+    {
+        IRoutingTable Map(Method method, string path, Func<Request, Response>responseFunction);
+    
+    }
+}
