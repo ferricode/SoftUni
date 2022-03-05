d6de0ef4bba1d032f2384af6ef99d1773e0ce577
ferricode <ferided88@gmail.com>
2022-03-05 21:05:51 +0200

    New Lab project Asp.Net Core-Warehouse. Views Added. Model binders added.

---
+++ ASP.NET/Warehouse/Warehouse/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+using Warehouse.Core.Constants;
+using Warehouse.Models;
+
+namespace Warehouse.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData[MessageConstant.SuccessMessage] = "Браво, успяхме да подкараме тостера!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+       /* public override async Task OnActionExecutinAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            //Executing
+            var executedContext=await next();
+
+            //Executed
+        }*/
+    }
+}
