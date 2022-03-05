fd41587882bc56129021eb1b9c5a519e19ae7c37
ferricode <ferided88@gmail.com>
2022-03-05 21:15:41 +0200

    Attribute to check if date is before another date i added. Constants added.

---
+++ ASP.NET/Warehouse/Warehouse.Core/CustomAttributes/IsBeforeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Warehouse.Core.CustomAttributes
+{
+    public class IsBeforeAttribute : ValidationAttribute
+    {
+        private readonly string _propertyToCompare;
+        public IsBeforeAttribute(string _propertyToCompare, string errorMessage = "")
+        {
+            this._propertyToCompare = _propertyToCompare;
+            this.ErrorMessage = errorMessage;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+          
+
+
+            try
+            {
+                DateTime dateToCompare = (DateTime)validationContext
+               .ObjectType
+               .GetProperty(_propertyToCompare)
+               .GetValue(validationContext.ObjectInstance);
+
+                if ((DateTime)value < dateToCompare)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (Exception ex)
+            {}
+
+            return new ValidationResult(ErrorMessage);
+        }
+
+   
+    }
+}
