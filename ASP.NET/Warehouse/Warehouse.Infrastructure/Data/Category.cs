a828f0af7176d4d7caabe4c37bd63f37724f197c
ferricode <ferided88@gmail.com>
2022-03-10 00:43:47 +0200

    Data models added.

---
+++ ASP.NET/Warehouse/Warehouse.Infrastructure/Data/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Infrastructure.Data
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(100)]
+        public string  Label { get; set; }
+  
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName ="date")]
+        public DateTime DateFrom { get; set; } =DateTime.Today;
+        [Column(TypeName = "date")]
+        public DateTime DateTo { get; set; } = DateTime.Today;
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+
+    }
+}
