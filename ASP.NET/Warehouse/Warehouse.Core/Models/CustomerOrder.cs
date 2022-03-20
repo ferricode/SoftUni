ba2a23ec954871f6a94cdd848e3e7a017d1bd64e
ferricode <ferided88@gmail.com>
2022-03-20 14:40:45 +0200

    Api added.

---
+++ ASP.NET/Warehouse/Warehouse.Core/Models/CustomerOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core.Models
+{
+    /// <summary>
+    /// Поръчка
+    /// </summary>
+    public class CustomerOrder
+    {
+        /// <summary>
+        /// Клиентски номер
+        /// </summary>
+        public string CustomerNumber { get; set; }
+        /// <summary>
+        /// Списък с поръчки
+        /// </summary>
+        public List<ItemOrder> Items { get; set; } = new List<ItemOrder>();
+    }
+}
---
+++ ASP.NET/Warehouse/Warehouse.Core/Models/ItemOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core.Models
+{
+    /// <summary>
+    /// Поръчан продукт
+    /// </summary>
+    public class ItemOrder
+    {
+        /// <summary>
+        /// Баркод на продукта
+        /// </summary>
+        public string Barcode{ get; set; }
+        /// <summary>
+        /// Брой поръчани продукти
+        /// </summary>
+        public int Count { get; set; }
+    }
+}
