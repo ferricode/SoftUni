6fa9e1a9e3dd04bb7da6947d0b271347dabfc13a
ferricode <ferided88@gmail.com>
2022-03-20 14:23:35 +0200

    User constants added.

---
+++ ASP.NET/Warehouse/Warehouse.Core/Constants/UserConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core.Constants
+{
+    public static class UserConstants
+    {
+   
+        public static class Roles
+        {
+            public const string Administrator = "Administrator";
+            public const string Manager = "Administrator, Manager";
+        }
+    }
+}
