9f5cf23c04d9fdf7c11bc5702beafa22634b929e
ferricode <ferided88@gmail.com>
2022-03-20 14:22:47 +0200

    UserController Added

---
+++ ASP.NET/Warehouse/Warehouse/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Core;
+using Warehouse.Core.Constants;
+using Warehouse.Core.Contracts;
+using Warehouse.Infrastructure.Data.Identity;
+
+namespace Warehouse.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private IUserService service;
+        public UserController(
+            RoleManager<IdentityRole> _roleManager,
+            UserManager<ApplicationUser> _userManager,
+            IUserService _service)
+        {
+            roleManager = _roleManager;
+            userManager = _userManager;
+            service = _service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = UserConstants.Roles.Administrator)]
+        public async Task<IActionResult> ManageUsers()
+        {
+            var users = await service.GetUsers();
+            return Ok(users);
+        }
+        public async Task<IActionResult> CreateRole()
+        {
+            await roleManager.CreateAsync(new IdentityRole()
+            {
+                Name = "Administrator"
+            });
+
+            return Ok();
+        }
+    }
+}
