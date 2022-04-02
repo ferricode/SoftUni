88d9e4b4d203dce7b77606c99fa4bbb8eeda15bc
ferricode <ferided88@gmail.com>
2022-04-02 21:35:52 +0300

    Clear usings.

--- ASP.NET/TaxFairy/TaxFairy.Infrastructure/Data/ApplicationDbContext.cs
+++ ASP.NET/TaxFairy/TaxFairy.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using TaxFairy.Infrastructure.Data.Enums;
 using TaxFairy.Infrastructure.Data.Models;
 
 namespace TaxFairy.Infrastructure.Data
