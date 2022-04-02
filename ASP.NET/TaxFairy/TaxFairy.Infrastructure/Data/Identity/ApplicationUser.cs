8045c2fb085450bab352b8005325345583a83066
ferricode <ferided88@gmail.com>
2022-04-02 21:37:37 +0300

    ApplicationUser created.

---
+++ ASP.NET/TaxFairy/TaxFairy.Infrastructure/Data/Identity/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxFairy.Infrastructure.Data.Models;
+
+namespace TaxFairy.Infrastructure.Data.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [StringLength(60, MinimumLength =5)]
+        public string? FullName { get; set; }
+        public IList<Vendor> Vendors { get; set; }
+        public ApplicationUser()
+        {
+            Vendors = new List<Vendor>();
+        }
+    }
+}
