e47e37d6914ed0a854467b05f24ca543147cd319
ferricode <ferided88@gmail.com>
2022-04-02 21:54:16 +0300

    ApplicationUser created.

--- ASP.NET/TaxFairy/TaxFairy.Infrastructure/Data/ApplicationDbContext.cs
+++ ASP.NET/TaxFairy/TaxFairy.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TaxFairy.Infrastructure.Data.Identity;
 using TaxFairy.Infrastructure.Data.Models;
 
 namespace TaxFairy.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
