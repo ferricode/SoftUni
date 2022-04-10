c0616fbd380caec30d95811da74bcc83907c4be3
ferricode <ferided88@gmail.com>
2022-04-11 00:36:44 +0300

    Validation Service.

---
+++ ASP.NET/TaxFairy/TaxFairy.Core/Contracts/IValidationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxFairy.Core.Contracts
+{
+    public interface IValidationService
+    {
+        public interface IValidationService
+        {
+            (bool isValid, string error) ValidateModel(object model);
+        }
+    }
+}
