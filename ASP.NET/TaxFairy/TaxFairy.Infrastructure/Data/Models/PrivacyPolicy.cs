bcdd106c0f6353d27ee1bea964bb10bfb90599ac
ferricode <ferided88@gmail.com>
2022-04-16 20:01:52 +0300

    Added PrivacyPolicy table.

---
+++ ASP.NET/TaxFairy/TaxFairy.Infrastructure/Data/Models/PrivacyPolicy.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TaxFairy.Infrastructure.Data.Models
+{
+    using static DataConstants;
+    public class PrivacyPolicy
+    {
+        [Key]
+        [MaxLength(IdMaxLength)]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime LastUpdated { get; set; }
+
+    }
+}
---
+++ ASP.NET/TaxFairy/TaxFairy.Infrastructure/Migrations/20220416165826_PrivacyPolicyMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaxFairy.Infrastructure.Data.Migrations
+{
+    public partial class PrivacyPolicyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PrivacyPolicies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", maxLength: 40, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrivacyPolicies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PrivacyPolicies");
+        }
+    }
+}
