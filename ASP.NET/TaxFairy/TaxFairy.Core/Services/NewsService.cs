d743c63dffacfa510eb72b6c7216e1833244ed5d
ferricode <ferided88@gmail.com>
2022-04-24 01:38:47 +0300

    NewsController in Admin with views done.

--- ASP.NET/TaxFairy/TaxFairy.Core/Services/NewsService.cs
+++ ASP.NET/TaxFairy/TaxFairy.Core/Services/NewsService.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaxFairy.Core.Contracts;
-using TaxFairy.Core.Models;
+using TaxFairy.Core.ViewModels;
 using TaxFairy.Infrastructure.Data.Models;
 using TaxFairy.Infrastructure.Repositories;
 
@@ -16,13 +17,26 @@ namespace TaxFairy.Core.Services
         private readonly ITaxFairyDbRepository repo;
 
 
+
         public NewsService(ITaxFairyDbRepository _repo)
         {
             repo = _repo;
+
         }
-        public Task<bool> EditNews(NewsViewModel model)
+        public async Task<bool> EditNews(NewsViewModel model)
         {
-            throw new NotImplementedException();
+            bool result = false;
+            var news = await repo.GetByIdAsync<News>(model.Id);
+
+            if (news != null)
+            {
+                news.Content = model.Content;
+
+                await repo.SaveChangesAsync();
+                result = true;
+            }
+
+            return result;
         }
 
         public async Task<IEnumerable<NewsViewModel>> GetNewsList()
@@ -31,7 +45,7 @@ namespace TaxFairy.Core.Services
             return await repo.All<News>()
                 .Select(n => new NewsViewModel()
                 {
-                    Id = n.Id.ToString(),
+                    Id = n.Id,
                     Content = n.Content,
                     IssueDate = n.IssueDate,
 
@@ -40,9 +54,38 @@ namespace TaxFairy.Core.Services
 
         }
 
-        public Task<NewsViewModel> GetNewsToEdit(string id)
+        public async Task<NewsViewModel> GetNewsToEdit(string id)
         {
-            throw new NotImplementedException();
+            var news = await repo.GetByIdAsync<News>(id);
+
+            return new NewsViewModel()
+            {
+                Id = news.Id,
+                Content = news.Content,
+                IssueDate = news.IssueDate.Date
+            };
+        }
+        public async Task<bool> CreateNews(NewsCreateViewModel model)
+        {
+            bool created = false;
+
+
+            var news = new News()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Content = model.Content,
+                IssueDate = DateTime.Now.Date,
+            };
+
+            if (news != null)
+            {
+
+                await repo.AddAsync(news);
+                await repo.SaveChangesAsync();
+                created = true;
+            }
+
+            return created;
         }
     }
 }
--- ASP.NET/TaxFairy/TaxFairy/Areas/Admin/Controllers/NewsController.cs
+++ ASP.NET/TaxFairy/TaxFairy/Areas/Admin/Controllers/NewsController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using TaxFairy.Core.Constants;
 using TaxFairy.Core.Contracts;
+using TaxFairy.Core.ViewModels;
+using TaxFairy.Models;
 
 namespace TaxFairy.Areas.Admin.Controllers
 {
@@ -18,6 +21,64 @@ namespace TaxFairy.Areas.Admin.Controllers
         {
             var news = await service.GetNewsList();
             return View(news);
+        }
+        public async Task<IActionResult> Edit(string id)
+        {
+            var model = await service.GetNewsToEdit(id);
+
+            return View(model);
+
+        }
+        public async Task<IActionResult> Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(NewsCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (await service.CreateNews(model))
+            {
+         
+                return Redirect("/Admin/News/ManageNews");
+            }
+            else
+            {
+                ViewData[MessageConstant.ErrorMessage] = "Възникна грешка!";
+                return View(model);
+
+            }
+
+
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(NewsViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (await service.EditNews(model))
+            {
+                ViewData[MessageConstant.SuccessMessage] = "Промените бяха записани успешно.";
+               
+            }
+            else
+            {
+                ViewData[MessageConstant.ErrorMessage] = "Възникна грешка!";
+          
+
+            }
+
+            return View(model);
+
         }
     }
 }
