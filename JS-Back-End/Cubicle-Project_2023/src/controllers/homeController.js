627e832f2314123a5fa6d7312d4b91c910f482a8
ferricode <ferided88@gmail.com>
2023-01-26 19:28:43 +0200

    Show homePage with dataBase cubes.

--- JS-Back-End/Cubicle-Project_2023/src/controllers/homeController.js
+++ JS-Back-End/Cubicle-Project_2023/src/controllers/homeController.js
@@ -1,14 +1,16 @@
-const db = require('../db.json');
+//const db = require('../db.json');
+const Cube = require('../models/Cube');
 
-exports.getHomePage = (req, res) => {
+exports.getHomePage = async (req, res) => {
     const { search, from: difficultyFrom, to: difficultyTo } = req.query;
-    let cubes = db.cubes;
+    let cubes = await Cube.find().lean();
 
+    //TODO use db filtration instead of inmemory filtering
     if (search) {
         cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
     }
     if (difficultyFrom) {
-        cubes = cubes.filter(cube => cube.difficultyLevel >= difficultyFrom);
+        cubes = cubes.filter(cube => cube.difficultyLevel >= difficultyFrom); w;
     }
     if (difficultyTo) {
         cubes = cubes.filter(cube => cube.difficultyLevel <= difficultyTo);
