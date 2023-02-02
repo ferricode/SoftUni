aceced5c30f64c0efb4744b41d7b90600e4ece44
ferricode <ferided88@gmail.com>
2023-02-02 22:37:30 +0200

    Added accessory to DB.

--- JS-Back-End/Cubicle-Project_2023/src/controllers/accessoryController.js
+++ JS-Back-End/Cubicle-Project_2023/src/controllers/accessoryController.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 
+const Accessory = require('../models/Accessory');
+
 //URL: /accessory/create
 router.get('/create', (req, res) => {
     res.render('accessory/create');
+});
+router.post('/create', async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+    //Var.1
+    // const accessory=new Accessory();
+
+    //Var.2
+    await Accessory.create({ name, description, imageUrl });
+    res.redirect('/');
 });
 
 module.exports = router;
