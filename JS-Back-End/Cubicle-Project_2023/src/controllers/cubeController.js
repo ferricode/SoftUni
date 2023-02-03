9d3bc30d61f7f58d00db697e41d408b9601b1cbd
ferricode <ferided88@gmail.com>
2023-02-03 18:05:47 +0200

    Added accessories to cube.

--- JS-Back-End/Cubicle-Project_2023/src/controllers/cubeController.js
+++ JS-Back-End/Cubicle-Project_2023/src/controllers/cubeController.js
@@ -17,18 +17,29 @@ exports.postCreateCube = async (req, res) => {
     res.redirect('/');
 };
 exports.getCubeDetails = async (req, res) => {
-    const cube = await Cube.findById(req.params.cubeId).lean();
+    const cube = await Cube.findById(req.params.cubeId).populate('accessories').lean();
 
     if (!cube) {
         return res.redirect('/404');
     }
 
-    res.render('details', { cube });
+    res.render('cube/details', { cube });
 };
+
 
 exports.getAttachAccessory = async (req, res) => {
     const cube = await Cube.findById(req.params.cubeId).lean();
-    const accessories = await Accessory.find().lean();
+    const accessories = await Accessory.find({ _id: { $nin: cube.accessories } }).lean();
 
     res.render('cube/attach', { cube, accessories });
+};
+
+exports.postAttachAccessory = async (req, res) => {
+    const cube = await Cube.findById(req.params.cubeId);
+    const accessoryId = req.body.accessory;
+    cube.accessories.push(accessoryId);
+
+    await cube.save();
+
+    res.redirect(`/cubes/${cube._id}/details`);
 };
