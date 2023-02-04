7ba477b3447183d93214d881105ca911324cc01c
ferricode <ferided88@gmail.com>
2023-02-04 21:35:07 +0200

    Added session, dataService, usercheck.

--- JS-Back-End/Cookies/index.js
+++ JS-Back-End/Cookies/index.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-//const { urlencoded } = require('express/lib/express');
+const expressSession = require('express-session');
+
+const dataService = require('./dataService');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
 app.get('/', (req, res) => {
     res.send(`
             <h1>Hello from Home Page</h1>
             
+            <p><a href="/register">Register<a/>
             <p><a href="/login">Login<a/>
+            <p><a href="/logout">Logout<a/>
             <p><a href="/profile">Profile<a/>
         `);
 });
 
 app.get('/login', (req, res) => {
     res.send(`
+    <h1>Sign In</h1>
         <form method="POST">
             <label for="username">Username</label>
             <input type="text" id="username" name="username" />
@@ -31,18 +42,48 @@ app.get('/login', (req, res) => {
 });
 
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
-    if (username == 'Feride' && password == 'kafe') {
+    try {
+        const user = await dataService.loginUser(username, password);
         const authData = {
-            username: 'Feride',
+            username: user.username,
         };
 
         res.cookie('auth', JSON.stringify(authData));
+        req.session.username = user.username;
+        req.session.privateInfo = user.password;
+
         return res.redirect('/');
+    } catch (err) {
+        console.log(err);
+        res.status(401).end();
     }
+
     res.status(401).end();
+});
+
+app.get('/register', (req, res) => {
+    res.send(`
+        <h1>Sign Up</h1>
+        <form method="POST">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" />
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" />
+
+            <input type="submit" value="register" />
+        </form>
+    `);
+});
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    await dataService.registerUser(username, password);
+
+    res.redirect('/login');
 });
 
 app.get('/profile', (req, res) => {
@@ -53,10 +94,13 @@ app.get('/profile', (req, res) => {
         return res.status(401).end();
     }
     const { username } = JSON.parse(authData);
-    console.log(username);
+    console.log(req.session);
 
     res.send(`
         <h2>Hello ${username}</h2>`);
 });
-
+app.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/');
+});
 app.listen(5000, () => console.log('Server is listening on port 5000...'));
