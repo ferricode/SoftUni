d8ed2a3371cec5c4dfc1a52285da1b3ccbb197a6
ferricode <ferided88@gmail.com>
2023-02-05 19:40:01 +0200

    Send cookie to client.

--- JS-Back-End/Cubicle-Project_2023/src/controllers/authController.js
+++ JS-Back-End/Cubicle-Project_2023/src/controllers/authController.js
@@ -10,7 +10,8 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     try {
         const token = await authService.login(username, password);
-        console.log(token);
+
+        res.cookie('auth', token, { httpOnly: true });
     } catch (err) {
         console.log(err);
         return res.redirect('/');
