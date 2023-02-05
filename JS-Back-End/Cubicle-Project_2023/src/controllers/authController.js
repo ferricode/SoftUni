9420e355c1bf834d5e262d556d1b0d92309268aa
ferricode <ferided88@gmail.com>
2023-02-05 17:54:05 +0200

    Check user and pass on login.

--- JS-Back-End/Cubicle-Project_2023/src/controllers/authController.js
+++ JS-Back-End/Cubicle-Project_2023/src/controllers/authController.js
@@ -1,18 +1,41 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
-router.get('/login', (rq, res) => {
+router.get('/login', (req, res) => {
     res.render('auth/login');
 });
-router.get('/register', (rq, res) => {
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await authService.login(username, password);
+        console.log(user);
+    } catch (err) {
+        console.log(err);
+        return res.redirect('/');
+    }
+    res.redirect('/');
+});
+
+router.get('/register', (req, res) => {
     res.render('auth/register');
 });
-router.post('/register', (rq, res) => {
+
+router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
 
-    if (password != repeatPassword) {
-        return res.status(404).end();
+    if (password !== repeatPassword) {
+        return res.redirect('/404');
     }
+    const existingUser = await authService.getUserByUsername(username);
 
+    if (existingUser) {
+        return res.redirect('/404');
+    }
+    const user = await authService.register(username, password);
+
+    console.log(user);
+    res.redirect('/login');
 
 });
 
