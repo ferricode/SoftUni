10f0334251163bf93fbbac5e01f59c7f5269e044
ferricode <ferided88@gmail.com>
2023-02-06 14:05:28 +0200

    Added Logout.

--- JS-Back-End/Cubicle-Project_2023/src/controllers/authController.js
+++ JS-Back-End/Cubicle-Project_2023/src/controllers/authController.js
@@ -40,5 +40,9 @@ router.post('/register', async (req, res) => {
     res.redirect('/login');
 
 });
+router.get('/logout', (req, res) => {
+    res.clearCookie('auth');
 
+    res.redirect('/');
+});
 module.exports = router;
