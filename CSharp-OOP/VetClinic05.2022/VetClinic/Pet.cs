2bd14243372a321754e1552232e4ad905b692cf8
ferricode <ferided88@gmail.com>
2023-02-27 00:21:46 +0200

    Exercises from ExamPrep course 05.2022

---
+++ CSharp-OOP/VetClinic05.2022/VetClinic/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+        public Pet(string name, int age, string owner)
+        {
+            Name = name;
+            Age = age;
+            Owner = owner;
+        }
+
+        public string  Name { get; set; }
+        public int Age { get; set; }
+        public string Owner { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age} Owner: {Owner}";
+        }
+    }
+}
---
+++ CSharp-OOP/VetClinic05.2022/VetClinic05.2022/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+//hats - stack
+//scarfs - queue
+
+/*if(fats>scarfs)
+ * new set - > hats+scarfs
+ * remove hats scarfs elements
+ * else if(scarfs > hats)
+ * remove hats
+ * else
+ * remove scarfs
+ * element of hats +1
+ * print
+ */
+
+int[] inputHats = Console.ReadLine().Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+int[] inputScars = Console.ReadLine().Split(" ")
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> hats = new Stack<int>(inputHats);
+Queue<int> scarfs = new Queue<int>(inputScars);
+List<int> elements = new List<int>();
+
+while (hats.Count > 0 && scarfs.Count > 0)
+{
+    int hat = hats.Peek();
+    int scarf = scarfs.Peek();
+
+    if (hat > scarf)
+    {
+        elements.Add(hat + scarf);
+        hats.Pop();
+        scarfs.Dequeue();
+    }
+    else if (hat < scarf)
+    {
+        hats.Pop();
+    }
+    else {
+        scarfs.Dequeue();
+        int value = hats.Pop() + 1;
+        hats.Push(value);
+    }
+
+}
+    Console.WriteLine($"The most expensive set is: {elements.Max()}");
+    Console.WriteLine(string.Join(" ",elements));
