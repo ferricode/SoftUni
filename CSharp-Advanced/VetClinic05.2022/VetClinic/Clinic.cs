16238166f8b1ca71b76d1aae412461a81cf265c7
ferricode <ferided88@gmail.com>
2023-02-28 10:36:19 +0200

    Added VetClinic, Warship and WarmWinter ex.

---
+++ CSharp-Advanced/VetClinic05.2022/VetClinic/Clinic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> pets;
+
+        private int capacity;
+
+        public Clinic(int capacity)
+        {
+            this.pets = new List<Pet>();
+            this.capacity = capacity;
+        }
+
+        public int Capacity { get; set; }
+        public int Count => pets.Count;
+        public void Add(Pet pet)
+        {
+            if (pets.Count < capacity)
+            {
+                pets.Add(pet);
+            }
+        }
+        public bool Remove(string name)
+        {
+            Pet pet = pets.FirstOrDefault(p => p.Name == name);
+            return pets.Remove(pet);
+        }
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = pets.FirstOrDefault(pet => pet.Owner == owner && pet.Name == name);
+
+            return pet;
+        }
+        public Pet GetOldestPet()
+        {
+            Pet pet = pets.OrderByDescending(p => p.Age).FirstOrDefault();
+            return pet;
+        }
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+
+            foreach (Pet pet in pets)
+            {
+                sb.AppendLine($"Pet { pet.Name} with owner: { pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
