0617e1fab4897537a8feabe429a6e02028d91c5e
ferricode <ferided88@gmail.com>
2023-03-02 23:33:55 +0200

    User List workshop with React.

---
+++ JS-React/React-User-List-Workshop/client/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+import * as userService from './services/userService';
+
+import { Fragment } from 'react';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Search } from './components/Search';
+import { UserList } from './components/UserList';
+import './App.css';
+
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll()
+      .then(setUsers)
+      .catch(err => {
+        console.log('Error' + err);
+      });
+  }, []);
+  return (
+    <Fragment>
+      <Header />
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+
+          <UserList users={users} />
+        </section>
+      </main>
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ JS-React/React-User-List-Workshop/client/src/services/userService.js
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:3005/api/users';
+
+export const getAll = async () => {
+    const response = await fetch(`${baseUrl}`);
+    const result = await response.json();
+
+    return result.users;
+
+};
+
+export const getOne = async (userId) => {
+    const response = await fetch(`${baseUrl}/${userId}`);
+    const result = await response.json();
+    console.log(result.user);
+    return result.user;
+}; 
