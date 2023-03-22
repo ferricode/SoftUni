ab3ce20eec8833e092414acde3b95552a5746113
ferricode <ferided88@gmail.com>
2023-03-22 19:54:20 +0200

    Workshops for Routing, Hooks, Authentication, Advanced tech..

---
+++ JS-React/GamesPlay-Routing-Hooks-Auth-Advanced/games-play/src/Resource.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div id="box">
+      {/* <!-- Edit Page ( Only for the creator )--> */}
+
+      {/* <!--Details Page--> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ JS-React/GamesPlay-Routing-Hooks-Auth-Advanced/games-play/src/hooks/useForm.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export const useForm = (initialValues, onSubmitHandler) => {
+    const [values, setValues] = useState(initialValues);
+
+    const changeHandler = (e) => {
+        setValues(state => ({ ...state, [e.target.name]: e.target.value }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        onSubmitHandler(values);
+    };
+
+    const changeValues = (newValues) => {
+        //TODO:Validate newValues shape (shouldd be like initialValues)
+
+        setValues(newValues);
+    };
+
+    return {
+        values,
+        changeHandler,
+        onSubmit,
+        changeValues,
+    };
+};
